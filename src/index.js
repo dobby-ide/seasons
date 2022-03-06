11f995c35b00b8847a2915b4792c3c69d4f4cdee
Fabio Privitera <fabio_privitera@yahoo.it>
2022-03-06 11:29:02 +0200

    conditional logic add to src/index.js

--- src/index.js
+++ src/index.js
@@ -5,14 +5,27 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: 55 };
+    this.state = { lat: null, errorMessage: '' };
+
+     window.navigator.geolocation.getCurrentPosition(
+       (position) => {
+         this.setState({lat: position.coords.latitude})
+       },
+       (err) =>{
+
+        this.setState({errorMessage: err.message})
+       }
+     );
   }
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
-    return <div>latitude: {this.state.lat}</div>;
+   if(this.state.errorMessage && !this.state.lat){
+     return <div>Error: {this.state.errorMessage}</div>
+   }
+   if(!this.state.errorMessage && this.state.lat){
+     return <div>latitude: {this.state.lat}</div>
+   }
+   return <div>loading.................</div>
+   
   }
 }
 
