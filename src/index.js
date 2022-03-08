a0655ab11571ae75895a6e760d55ba6321d8354f
Fabio Privitera <fabio_privitera@yahoo.it>
2022-03-08 17:43:36 +0200

    adding src/SeasonDisplay.js, adding componentDidMount to index.js, README.md updates

--- src/index.js
+++ src/index.js
@@ -1,31 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import SeasonDisplay from './SeasonDisplay';
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { lat: null, errorMessage: '' };
-
-     window.navigator.geolocation.getCurrentPosition(
-       (position) => {
-         this.setState({lat: position.coords.latitude})
-       },
-       (err) =>{
-
-        this.setState({errorMessage: err.message})
-       }
-     );
+  state = { lat: null, errorMessage: '' };
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
   }
+  //   componentDidUpdate(){
+  //     console.log("component rerendered")
+  //   }
   render() {
-   if(this.state.errorMessage && !this.state.lat){
-     return <div>Error: {this.state.errorMessage}</div>
-   }
-   if(!this.state.errorMessage && this.state.lat){
-     return <div>latitude: {this.state.lat}</div>
-   }
-   return <div>loading.................</div>
-   
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+    return <div>locading..</div>;
   }
 }
 
